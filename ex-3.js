ecd05643b2ebcb7f6655700cce05dd24d9c99ead
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-23 18:01:59 +0700

    fix(ex-3): edit assignmemt ex-3

--- ex-3.js
+++ ex-3.js
@@ -1,16 +1,31 @@
 function isPlainObject(value) {
-  let temp = Object(value);
-  return (temp.name === "John")
+
+  if(typeof(value) === "object"){
+    if (Array.isArray(value) === true || value === null){
+      return false
+    }
+    else {
+      return true
+    }
+  }
+  else {
+    return "This is not an object"
+  }
+
 }
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
 let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
+let result4 = isPlainObject(undefined); // test undefined
+let result5 = isPlainObject({ key: "asd" }); //test anathor object is true
 
 // ผลลัพธ์ควรที่ได้จาก Example case
 console.log(result1); //true
 console.log(result2); // false
 console.log(result3); // false
+console.log(result4);
+console.log(result5);
 
 
