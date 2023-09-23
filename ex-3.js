579ac7abb0ceca785cc0309b376c789562091c14
Supakit <book0970403815@hotmail.com>
2023-09-23 22:31:15 +0700

    fix : book send assisgnment ex(1-3)

--- ex-3.js
+++ ex-3.js
@@ -1,12 +1,11 @@
-function isPlainObject(value) {
-  // Start coding here
-}
-
+function isPlainObject (valse) {
+  return (typeof(valse)) === "object" && valse !== null && true !== Array.isArray(valse)   
+    }
 // Example case
-let result1 = isPlainObject({ name: "John" });
+let result1 = isPlainObject({ name : "John" });
 let result2 = isPlainObject(null);
-let result3 = isPlainObject(["apples", "oranges"]);
-
+let result3 = isPlainObject(["apples","oranges"]);
+let result4 = isPlainObject(undefined) 
 // ผลลัพธ์ควรที่ได้จาก Example case
 console.log(result1); //true
 console.log(result2); // false
